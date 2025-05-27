ec3347172e0892d114916ad69ae1f0b22da88172
Maha Amer <maha@noury.ch>
2025-05-27 06:06:48 +0300

    add Skills section to homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import About from "@/components/sections/About"
 import Hero from "@/components/sections/Hero"
+import Skills from "@/components/sections/Skills"
 
 export default function Home() {
 	return (
 		<main>
 			<Hero />
 			<About />
-			{/* <Skills /> */}
+			<Skills />
 			{/* <Projects />
 			<Experience />
 			<CertificationsSection />
